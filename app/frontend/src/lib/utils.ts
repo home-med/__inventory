009a604b4acf5075c5662092e23e7334eaf7a5ef
James Rhamy <jrhamy@broadwayhomemed.com>
2023-06-07 10:29:14 -0500

    refactor: :fire: Removed uuid

--- app/frontend/src/lib/utils.ts
+++ app/frontend/src/lib/utils.ts
@@ -1,21 +1,16 @@
-import { browser } from '$app/environment';
 import type PocketBase from 'pocketbase';
 
 export const dbAdd = (pb: PocketBase, table: string, values: FormDataEntryValue[]) => {
   if (!values) values = [];
   return Promise.allSettled(values.map(async item => {
-    return await pb.collection(table).create(item, {$autoCancel: false});
+    return await pb.collection(table).create(item, { $autoCancel: false });
   }));
 }
 
-export const uuid = () => {
-  if (browser) return crypto.randomUUID();
-  return '';
-}
 
 
 export const cmp = (item1: unknown, item2: unknown) => {
-  if(typeof item1 === 'string' && typeof item2 === 'string') return item1.toLowerCase() === item2.toLowerCase();
+  if (typeof item1 === 'string' && typeof item2 === 'string') return item1.toLowerCase() === item2.toLowerCase();
   return item1 === item2;
 }
 
@@ -55,4 +50,17 @@ export const throttle = (cb: Function, delay: number = 250) => {
     shouldWait = true;
     setTimeout(timeoutFunc, delay)
   }
+}
+
+export const batchPromiseProcessing = async (collection: any[], limit: number = 100, fn: Function) => {
+  let ret = [];
+  const batches = collection.reduce(
+    (acc: any[], _: any, i: number) => (i % limit) ? acc : [...acc, collection.slice(i, i + limit)], []
+  );
+  for (let i = 0, l = batches.length; i < l; i++) {
+    console.log(`Starting batch ${i+1} of ${l}`);
+    ret[i] = await Promise.allSettled(batches[i].map((item: any) => fn(item)));
+    console.log('Batch completed.')
+  }
+  return ret.flat(2);
 }
