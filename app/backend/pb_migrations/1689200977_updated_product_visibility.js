2151aaf532b220809032e292af6f5af83b5efcc9
James Rhamy <jrhamy@broadwayhomemed.com>
2023-07-12 18:01:07 -0500

    update database

---
+++ app/backend/pb_migrations/1689200977_updated_product_visibility.js
@@ -0,0 +1,45 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("womgti1ltdirstt")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ar9chftz",
+    "name": "location",
+    "type": "relation",
+    "required": true,
+    "unique": false,
+    "options": {
+      "collectionId": "216y102su3jrdtq",
+      "cascadeDelete": true,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": []
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("womgti1ltdirstt")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ar9chftz",
+    "name": "location",
+    "type": "relation",
+    "required": true,
+    "unique": false,
+    "options": {
+      "collectionId": "216y102su3jrdtq",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": []
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
